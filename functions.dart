38565ece9ff000122e99af46a84b6a2383f2fd8c
Lonnet12345 <lonnetniwahereza@gmail.com>
2023-07-14 14:36:00 +0300

    functions

--- functions.dart
+++ functions.dart
@@ -13,26 +13,54 @@ void main() {
 //
 //
   // print(myFunc);
-  getUserNames();
-  getAge();
+  // getUserNames();
+  // getAge();
+  // getName2();
+  // String myName = getName2();
+  // print(myName);
+
+  // call getName function
+  String myName = getName3();
+  // print("our output is $myName");
+  int myAge = getMyage();
+  print("my name is $myName and i am $myAge years old");
 }
 
-void myFunc() {
-  print("my programing is  the best");
+// void myFunc() {
+//   print("my programing is  the best");
+// }
+
+// void getUserNames() {
+//   print("Whats your name ?");
+//   String? name = stdin.readLineSync();
+//   // var name = stdin.readLineSync();
+//   print("My name is ${name!.toUpperCase()}");
+//   // getAge function();
+// }
+
+// void getAge() {
+//   print("How old are you ?");
+
+//   var age = stdin.readLineSync();
+//   print(age!.toUpperCase());
+//   print("My Age is:$age");
+// }
+
+String getName2() {
+  // print("getName");
+  return "Naki";
 }
 
-void getUserNames() {
-  print("Whats your name ?");
-  String? name = stdin.readLineSync();
-  // var name = stdin.readLineSync();
-  print("My name is ${name!.toUpperCase()}");
-  // getAge function();
+String getName3() {
+  // print("hey people? ?");
+  var name = stdin.readLineSync();
+  //  print(name!.toUpperCase());677
+  return name ?? "";
 }
 
-void getAge() {
-  print("How old are you ?");
-
+int getMyage() {
+  // print("How old are you ?");
   var age = stdin.readLineSync();
-  print(age!.toUpperCase());
   print("My Age is:$age");
+  return int.parse(age ?? "0");
 }
